bee309daf4bfdff74eb79f7d37b4419af6d285f5
mossab-alhunaiti <mossab99@gmail.com>
2026-02-12 16:32:50 +0300

    delete

--- debug-roles.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once('../../../wp-load.php');
-if (current_user_can('manage_options')) {
-    header('Content-Type: text/plain');
-    print_r(array_keys(wp_roles()->roles));
-} else {
-    echo "Unauthorized";
-}
